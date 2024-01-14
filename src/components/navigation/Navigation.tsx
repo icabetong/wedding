0aa776fefc4932e68a17210d47f1914ea5f86f40
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2024-01-14 13:13:21 +0800

    final sidebar:

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,16 +1,19 @@
-import React, { Fragment } from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { Popover, Transition } from '@headlessui/react'
-import { IconMenu, IconMoon, IconSun } from '@tabler/icons-react'
+import { IconMenu, IconMoon, IconSun, IconX } from '@tabler/icons-react'
+import { motion } from 'framer-motion'
+import Sidebar from '@/components/sidebar'
 import links from '@/shared/const/navigation'
 import useTheme from '@/shared/hooks/use-themes'
 import Item from './item'
 
 export default function Navigation() {
+  const [open, setOpen] = useState(false)
   const { change, theme } = useTheme()
 
   const handleThemeChange = () => change(theme === 'dark' ? 'light' : 'dark')
+  const handleSidebar = () => setOpen((prev) => !prev)
 
   return (
     <header className='border-b border-background-secondary dark:border-background-secondary-dark'>
@@ -44,31 +47,35 @@ export default function Navigation() {
           </button>
         </div>
         <div className='md:hidden'>
-          <Popover className='relative'>
-            <Popover.Button className='button-ghost'>
-              <span className='sr-only'>Links</span>
-              <IconMenu className='h-5 w-5' />
-            </Popover.Button>
-            <Transition
-              as={Fragment}
-              enter='transition ease-out duration-200'
-              enterFrom='opacity-0 translate-y-1'
-              enterTo='opacity-100 translate-y-0'
-              leave='transition ease-in duration-150'
-              leaveFrom='opacity-100 translate-y-0'
-              leaveTo='opacity-0 translate-y-1'
-            >
-              <Popover.Panel className='absolute right-0 z-20 w-screen max-w-xs mt-4'>
-                <div className='bg-background border border-background rounded-lg shadow'>
-                  <ul className='flex items-center flex-col'>
-                    {links.map((e) => (
-                      <Item key={e.key} href={e.key} label={e.data} />
-                    ))}
-                  </ul>
-                </div>
-              </Popover.Panel>
-            </Transition>
-          </Popover>
+          <button type='button' onClick={handleSidebar}>
+            <IconMenu className='h-5 w-5' />
+            <span className='sr-only'>Open Sidebar</span>
+          </button>
+          <Sidebar className='md:hidden' open={open}>
+            <div>
+              <button
+                type='button'
+                className='button-ghost'
+                onClick={handleSidebar}
+              >
+                <IconX className='h-5 w-5' />
+                <span className='sr-only'>Close sidebar</span>
+              </button>
+            </div>
+            <ul className='p-4 space-y-2'>
+              {links.map((e) => (
+                <motion.li
+                  key={e.key}
+                  className='w-full'
+                  whileHover={{ y: -4 }}
+                >
+                  <Link className='block py-2' href={e.key}>
+                    {e.data}
+                  </Link>
+                </motion.li>
+              ))}
+            </ul>
+          </Sidebar>
         </div>
       </nav>
     </header>
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,26 @@
+import React, { type PropsWithChildren } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+type SidebarProps = {
+  open: boolean
+  className?: string
+} & PropsWithChildren &
+  Partial<typeof defaultProps>
+const defaultProps = {
+  className: '',
+}
+
+export default function Sidebar({ open, className, children }: SidebarProps) {
+  return (
+    <div
+      className={twMerge(
+        'fixed right-0 top-0 z-40 h-full w-[35vw] overflow-x-hidden overflow-y-scroll border dark:border-background-secondary-dark dark:text-text-dark bg-background-secondary dark:bg-background-secondary-dark text-gray-800 shadow duration-300 ease-in-out',
+        className,
+        open ? 'translate-x-0' : 'translate-x-full'
+      )}
+    >
+      {children}
+    </div>
+  )
+}
+Sidebar.defaultProps = defaultProps
